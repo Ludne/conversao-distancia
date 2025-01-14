ad12738de62300bea09150ce0a7f5eba25db98da
ludnemorais <ludnemorais@hotmail.com>
2025-01-14 13:18:18 -0300

    Create a dockerfile to inicialize app in conteiner docker

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request # type: ignore
 import logging
 import socket  # Módulo para obter informações do servidor
 
